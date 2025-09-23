13e2764d64a5eefc8560990670413f2b3b423e8c
xiokevc <xiokevc@gmail.com>
2025-09-23 10:43:40 -0400

    revised settings.py

--- tracker/utils.py
+++ tracker/utils.py
@@ -1,14 +1,14 @@
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
-from django.urls import reverse  # 🔄 import reverse to generate URLs
+from django.urls import reverse  
 
 def send_reminder_email(reminder):
     subject = f"Upcoming Service Reminder for {reminder.vehicle.make} {reminder.vehicle.model}"
     context = {
         'reminder': reminder,
         'user': reminder.vehicle.user,
-        'vehicle_url': reverse('vehicle-detail', kwargs={'pk': reminder.vehicle.pk}),  # 🔄 dash-style name
+        'vehicle_url': reverse('vehicle-detail', kwargs={'pk': reminder.vehicle.pk}),  
     }
     message = render_to_string('tracker/emails/reminder_email.txt', context)
     send_mail(
